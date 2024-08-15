76e1cb472c3570e399c6b3e36f764a72a8dfac09
chen <92xiaodao@gmail.com>
2024-08-15 22:20:02 +0800

    feat: ✨ update env types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,14 @@
 export interface TempInfo {
-  tempName: string;
-  fileName: string;
-  fullPath: string;
+	tempName: string;
+	fileName: string;
+	fullPath: string;
 }
 
 export type TempInfoList = Array<TempInfo>;
 
 export interface PickerOption {
-  title: string;
-  value: string;
+	title: string;
+	value: string;
 }
 
 export type PickerOptionList = Array<PickerOption>;
@@ -16,5 +16,9 @@ export type PickerOptionList = Array<PickerOption>;
 export type TempNameList = Array<string>;
 
 export interface Envs {
-  [key: string]: string | undefined;
+	[key: string]: string | undefined;
+
+	CREATE_TEMP_TEMPLATE_DIRECTORY: string | undefined;
+	CREATE_TEMP_OUTPUT_DIRECTORY: string | undefined;
+	CREATE_TEMP_OUTPUT_DIRECTORY_CHOICES: string | undefined;
 }
