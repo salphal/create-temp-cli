31e7a995345217e2a9b3487ec9c7160ed02189fa
chen <92xiaodao@gmail.com>
2024-08-16 16:53:36 +0800

    feat: ✨ add directory utils

--- utils/directory.ts
+++ utils/directory.ts
@@ -52,3 +52,41 @@ export async function createDirectory(baseDirectoryPath: string, folderName?: st
     }
   });
 }
+
+
+export function copy(src: string, dest: string) {
+  const stat = fs.statSync(src)
+  if (stat.isDirectory()) {
+    copyDir(src, dest)
+  } else {
+    fs.copyFileSync(src, dest)
+  }
+}
+
+export function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, {recursive: true})
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    const destFile = path.resolve(destDir, file)
+    copy(srcFile, destFile)
+  }
+}
+
+export function isEmpty(path: string) {
+  const files = fs.readdirSync(path)
+  return files.length === 0 || (files.length === 1 && files[0] === '.git')
+}
+
+export function emptyDir(dir: string) {
+  if (!fs.existsSync(dir)) {
+    return
+  }
+  for (const file of fs.readdirSync(dir)) {
+    if (file === '.git') {
+      continue
+    }
+    fs.rmSync(path.resolve(dir, file), {recursive: true, force: true})
+  }
+}
+
+
