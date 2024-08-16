ae06c4336a66f25f0b65612284607bfd0d0f8df4
chen <92xiaodao@gmail.com>
2024-08-16 16:53:17 +0800

    feat: ✨ add download util

--- utils/download.ts
+++ utils/download.ts
@@ -1,42 +1,169 @@
-// @ts-ignore
-import download from 'download-git-repo';
-import ora from 'ora';
-import {red, green} from 'kolorist';
+import downloadUrl from 'download';
+import gitClone from 'git-clone';
+import { rimraf } from 'rimraf'
 
-interface DownloadConfig {
-  /** 远程仓库地址 */
-  remote: string;
-  /** 远程分支名( 默认 master ) */
-  branch: string;
-  /** 输出的本地路径( 以当前脚本为根路径, 若没有该文件夹则会自动创建, 若有则可能会冲突 ) */
-  outputPath: string;
-  /** 是否使用 http 直接从 url 下载 */
-  isDirect?: boolean;
-  /** clone 配置对象( 请求头等 ) */
-  options?: any;
-}
+const rm = rimraf.sync;
 
-async function clone(config: DownloadConfig) {
-  const spinner = ora("Pulling code...").start();
-  const {remote, branch, outputPath, isDirect = true, options = {clone: true}} = config;
-  return new Promise((resolve, reject) => {
-    /**
-     * 使用 http 从 master 处的 Github 存储库下载
-     * ?[bitbucket|gitlab]:[auth]/[registry]#[branch]
-     *
-     * 使用 http 直接从 url 下载
-     * [direct]:[full_git_url]#[branch]
-     */
-    const url = `${isDirect ? 'direct:' : ''}${remote}#${branch}`;
-    download(url, outputPath, options, (err: any) => {
-      if (err) {
-        spinner.fail(red(err));
-        reject(err);
+/**
+ * 因为没有 es module 的倒出, 只能自己重新倒出
+ * download-git-repo
+ * https://gitlab.com/flippidippi/download-git-repo
+ */
+
+/**
+ * Download `repo` to `dest` and callback `fn(err)`.
+ *
+ * @param {String} repo
+ * @param {String} dest
+ * @param {Object} opts
+ * @param {Function} fn
+ */
+function download(repo: any, dest: string, opts: any, fn: Function) {
+  if (typeof opts === 'function') {
+    fn = opts
+    opts = null
+  }
+  opts = opts || {}
+  let clone = opts.clone || false
+  delete opts.clone
+
+  repo = normalize(repo)
+  let url = repo.url || getUrl(repo, clone)
+
+  if (clone) {
+    let cloneOptions = {
+      checkout: repo.checkout,
+      shallow: repo.checkout === 'master',
+      ...opts
+    }
+    gitClone(url, dest, cloneOptions, function (err) {
+      if (err === undefined) {
+        rm(dest + '/.git')
+        fn()
+      } else {
+        fn(err)
       }
-    });
-    spinner.succeed(green(`Success pull ${remote}/${branch}`));
-    resolve(0);
-  });
+    })
+  } else {
+    let downloadOptions = {
+      extract: true,
+      strip: 1,
+      mode: '666',
+      ...opts,
+      headers: {
+        accept: 'application/zip',
+        ...(opts.headers || {})
+      }
+    }
+    downloadUrl(url, dest, downloadOptions)
+      .then(function (data) {
+        fn()
+      })
+      .catch(function (err) {
+        fn(err)
+      })
+  }
 }
 
-export default clone;
+/**
+ * Normalize a repo string.
+ *
+ * @param {String} repo
+ * @return {Object}
+ */
+function normalize(repo: string) {
+  let regex = /^(?:(direct):([^#]+)(?:#(.+))?)$/
+  let match = regex.exec(repo)
+
+  if (match) {
+    let url = match[2]
+    let directCheckout = match[3] || 'master'
+
+    return {
+      type: 'direct',
+      url: url,
+      checkout: directCheckout
+    }
+  } else {
+    regex = /^(?:(github|gitlab|bitbucket):)?(?:(.+):)?([^/]+)\/([^#]+)(?:#(.+))?$/
+    match = regex.exec(repo)
+    if (!match) return;
+    let type = match[1] || 'github'
+    let origin = match[2] || null
+    let owner = match[3]
+    let name = match[4]
+    let checkout = match[5] || 'master'
+
+    if (origin == null) {
+      if (type === 'github') {
+        origin = 'github.com'
+      } else if (type === 'gitlab') {
+        origin = 'gitlab.com'
+      } else if (type === 'bitbucket') {
+        origin = 'bitbucket.org'
+      }
+    }
+
+    return {
+      type: type,
+      origin: origin,
+      owner: owner,
+      name: name,
+      checkout: checkout
+    }
+  }
+}
+
+/**
+ * Adds protocol to url in none specified
+ *
+ * @return {String}
+ * @param origin
+ * @param clone
+ */
+function addProtocol(origin: string, clone: string) {
+  if (!/^(f|ht)tps?:\/\//i.test(origin)) {
+    if (clone) {
+      origin = 'git@' + origin;
+    } else {
+      origin = 'https://' + origin;
+    }
+  }
+  return origin;
+}
+
+/**
+ * Return a zip or git url for a given `repo`.
+ *
+ * @param {Object} repo
+ * @param {String} clone
+ * @return {String}
+ */
+function getUrl(repo: any, clone: string) {
+  let url
+
+  // Get origin with protocol and add trailing slash or colon (for ssh)
+  let origin = addProtocol(repo.origin, clone)
+  if (/^git@/i.test(origin)) {
+    origin = origin + ':'
+  } else {
+    origin = origin + '/'
+  }
+
+  // Build url
+  if (clone) {
+    url = origin + repo.owner + '/' + repo.name + '.git'
+  } else {
+    if (repo.type === 'github') {
+      url = origin + repo.owner + '/' + repo.name + '/archive/' + repo.checkout + '.zip'
+    } else if (repo.type === 'gitlab') {
+      url = origin + repo.owner + '/' + repo.name + '/repository/archive.zip?ref=' + repo.checkout
+    } else if (repo.type === 'bitbucket') {
+      url = origin + repo.owner + '/' + repo.name + '/get/' + repo.checkout + '.zip'
+    }
+  }
+
+  return url
+}
+
+export default download;
