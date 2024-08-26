595459a20cfe4dc620091d6e8868b21d2d8eb9a8
chen <92xiaodao@gmail.com>
2024-08-26 10:45:36 +0800

    feat: ✨ update

--- __test__/table.mjs
+++ __test__/table.mjs
@@ -4,9 +4,9 @@ import { table } from 'table';
 // const { table } = require('table');
 
 const data = [
-  ['0A', '0B', '0C'],
-  ['1A', '1B', '1C'],
-  ['2A', '2B', '2C']
+  ['name', 'age', 'gender'],
+  ['alpha', '18', 'male'],
+  ['beta', '20', 'female'],
 ];
 
 console.log(table(data));
