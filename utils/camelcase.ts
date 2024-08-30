c80213244f9ca75618868b60812745fb8ad16b63
chen <92xiaodao@gmail.com>
2024-08-30 09:57:25 +0800

    feat: ✨ update utils

--- utils/camelcase.ts
+++ utils/camelcase.ts
@@ -1,15 +1,49 @@
+/**
+ * CamelCase
+ */
 export function CamelCase(fileName: string): string {
-  return fileName.replace(/(^\w)/, (match) => match.toUpperCase());
+  return fileName
+    .split(/[-_\s]/)
+    .map(v => v.replace(/(^\w)/, (match) => match.toUpperCase()))
+    .join("");
 }
 
+/**
+ * camelCase
+ */
 export function camelCase(fileName: string): string {
-  return fileName.replace(/(^\w)/, (match) => match.toLowerCase());
+  return fileName
+    .split(/[-_\s]/)
+    .map((v, i) => v.replace(/(^\w)/, (match) => i === 0 ? match.toLowerCase() : match.toUpperCase()))
+    .join("");
 }
 
+/**
+ * camel-case
+ */
 export function camelcase(fileName: string): string {
-  return fileName.toLowerCase();
+  return fileName
+    .split(/[-_\s]/)
+    .map(v => v.toLowerCase())
+    .join("-");
 }
 
+/**
+ * CAMEL_CASE
+ */
 export function CAMELCASE(fileName: string): string {
-  return fileName.replace('-', '_').toUpperCase();
+  return fileName
+    .split(/[-_\s]/)
+    .map(v => v.toUpperCase())
+    .join("_");
+}
+
+/**
+ * CAMEL_CASE -> CC
+ */
+export function SHORTCAMELCASE(fileName: string): string {
+  return fileName
+    .split(/[-_\s]/)
+    .map(v => v.slice(0, 1).toUpperCase())
+    .join("");
 }
