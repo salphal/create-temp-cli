494c9a9ff3b769ec63489e056bc2a9c6f544c8b5
chen <92xiaodao@gmail.com>
2024-09-04 15:53:53 +0800

    feat: ✨ add tar extra utils

---
+++ src/utils/tar-cmd.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { PathExtra } from '@utils/path-extra';
+
+export class TarCmd {
+  /**
+   * tar -czvf /path/to/dist.tar.gz -C $(dirname /path/to/dist) $(basename /path/to/dist)
+   */
+  static getTarCmd(src: string, dest: string | null = null): string {
+    const { dir, name } = path.parse(src);
+
+    const baseName = PathExtra.__basename(name);
+    const srcFullPath = path.join(dir, baseName);
+
+    let command = `tar -czvf ${dest}`;
+    const filter = ` -C $(dirname ${srcFullPath}) $(basename ${srcFullPath})`;
+
+    if (!dest) {
+      const destName = PathExtra.fixTarExt(name);
+      command = `tar -czvf ${path.join(dir, destName)}`;
+    }
+
+    command += filter;
+
+    return command;
+  }
+
+  /**
+   * tar -xzvf /path/to/dist.tar.gz -C /path/to
+   */
+  static getUnTarCmd(src: string, dest: string | null = null): string {
+    const { dir, name } = path.parse(src);
+
+    let command = `tar -xzvf ${src} -C ${dest}`;
+
+    if (!dest) {
+      const destName = PathExtra.fixTarExt(name);
+      command = `tar -xzvf ${path.join(dir, destName)} -C $(dirname ${src})`;
+    }
+    return command;
+  }
+}
