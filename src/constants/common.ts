e6e80739afb570de240e92a8302bba44c3644139
chen <92xiaodao@gmail.com>
2024-09-06 10:02:26 +0800

    fix: 🐛 add common constant variables

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,1 +1,13 @@
-export const CONFIG_BASE_NAME = '.front-cli';
+import { CLI_NAME } from '@constants/cli';
+
+export const CLI_CONFIG_FILE_NAME = `.${CLI_NAME}`;
+
+export const TEMPLATE_FILE_NAME = '__template__';
+
+export const OUTPUT_FILE_NAME = '__output__';
+
+export const TEMP_FILE_NAME = '.temp.env';
+
+export const PUBLISH_CONFIG_FILE_NAME = 'publish.config.json';
+
+export const repositoryGitUrl = 'https://github.com/salphal/dev-cli.git';
