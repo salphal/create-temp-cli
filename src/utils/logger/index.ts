f54d799e1c212d53ef1682b9bdb04fb477ffc946
chen <92xiaodao@gmail.com>
2024-09-06 13:33:29 +0800

    feat: ✨ add logger object

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -4,7 +4,7 @@ import { Banner } from './banner';
 import { Loading } from '@utils';
 
 export class Logger {
-  static prefix = '[ Log ]: ';
+  static prefix: string = '[ Log ]: ';
 
   static log(message: any, type: (str: string | number) => string, prefix?: string) {
     const pf = prefix || this.prefix;
@@ -25,6 +25,26 @@ export class Logger {
 
   static error(message: any, prefix?: string) {
     this.log(message, red, prefix);
+  }
+
+  static obj(message: string, type: (str: string | number) => string, ...objs: any[]) {
+    console.log(type(this.prefix + message), ...objs);
+  }
+
+  static infoObj(message: string, ...objs: any[]) {
+    this.obj(message, blue, ...objs);
+  }
+
+  static successObj(message: string, ...objs: any[]) {
+    this.obj(message, green, ...objs);
+  }
+
+  static warnObj(message: string, ...objs: any[]) {
+    this.obj(message, yellow, ...objs);
+  }
+
+  static errorObj(message: string, ...objs: any[]) {
+    this.obj(message, red, ...objs);
   }
 
   static table(
