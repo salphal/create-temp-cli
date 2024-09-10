cbd71cae3c48016a65b09f95adbe890d2e28370d
chen <92xiaodao@gmail.com>
2024-09-10 14:36:56 +0800

    fix: 🐛 update path extra

--- src/utils/path-extra.ts
+++ src/utils/path-extra.ts
@@ -1,4 +1,4 @@
-import path, { ParsedPath } from 'path';
+import path, { FormatInputPathObject, ParsedPath } from 'path';
 
 export class PathExtra {
   static resolve(...paths: string[]): string {
@@ -13,18 +13,33 @@ export class PathExtra {
     return path.parse(p);
   }
 
+  static format(pathConfig: FormatInputPathObject): string {
+    return path.format(pathConfig);
+  }
+
   static __basename(p: string): string {
     const { name, ext } = path.parse(p);
     if (/.*(\.tar)$/.test(name)) {
-      return name.slice(0, name.indexOf('.'));
+      return name.slice(0, name.indexOf('.tar'));
     } else if (ext === '.jar') {
       return name + ext;
     }
     return name;
   }
 
+  static __extname(p: string): string {
+    if (/.*(\.tar\.gz)$/.test(p)) {
+      return '.tar.gz';
+    }
+    return path.extname(p);
+  }
+
   static __dirname(p: string): string {
     return path.dirname(p);
+  }
+
+  static isAbsolute(p: string): boolean {
+    return path.isAbsolute(p);
   }
 
   static forceSlash(path: string) {
