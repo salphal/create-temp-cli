32586a828d8071aa696ae08e1c39861c1562cc35
chen <92xiaodao@gmail.com>
2024-09-10 14:43:07 +0800

    feat: ✨ add init cli

---
+++ src/clis/init/index.ts
@@ -0,0 +1,67 @@
+import {FrontCli, ResCode, StepList, StepScheduler} from "@utils";
+import {Envs} from "@type/env";
+
+interface IInitContext extends Envs  {
+}
+
+interface IInitOptions {
+  /** 外部注入的运行时上下文的数据 */
+  ctx: Envs & {};
+}
+
+export class InitCli extends FrontCli<IInitContext> {
+
+  context: IInitContext = {
+    argv: {},
+    __dirname: "",
+    __filename: "",
+    envs: {}
+  };
+
+  stepList: StepList = [
+    {
+      name: "step_01",
+      remark: ``,
+      callback: async (ctx: IInitContext) => {
+        const {} = this.context;
+        console.log('=> step_01', ctx);
+        return {
+          code: ResCode.next,
+          data: {}
+        }
+      }
+    },
+    {
+      name: "step_02",
+      remark: ``,
+      callback: async (ctx: IInitContext) => {
+        const {} = this.context;
+        console.log('=> step_02', ctx);
+        return {
+          code: ResCode.next,
+          data: {}
+        }
+      }
+    },
+    {
+      name: "step_03",
+      remark: ``,
+      callback: async (ctx: IInitContext) => {
+        const {} = this.context;
+        console.log('=> step_03', ctx);
+        return {
+          code: ResCode.end,
+          data: {}
+        }
+      }
+    }
+  ];
+
+  scheduler: StepScheduler;
+
+  constructor(options: IInitOptions) {
+    super(options);
+    this.context = {...this.context, ...options.ctx};
+    this.scheduler = new StepScheduler({stepList: this.stepList});
+  }
+}
