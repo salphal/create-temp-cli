e7f070c800f580676634ec23f99a716dc7a80a22
chen <92xiaodao@gmail.com>
2024-09-18 14:05:56 +0800

    feat: ✨ update custom cli config

--- src/utils/core/front-cli.ts
+++ src/utils/core/front-cli.ts
@@ -6,11 +6,11 @@ import path from 'path';
 import { Prompt, PromptList } from '@utils';
 import prompts from 'prompts';
 
-export interface ICustomCliConfig {
+export interface CustomCliConfig {
   [key: string]: any;
 
   /** 自定义输出前缀列表 */
-  outputPrefixList: Array<string>;
+  outputPrefixList: (ctx: any) => Array<string>;
   /** 自定义输出路径集合 */
   outputPathMap: (ctx: any) => { [key: string]: string };
   /** 自定义交互列表 */
@@ -22,6 +22,7 @@ export interface ICustomCliConfig {
 export interface IFrontCliOptions {
   [key: string]: any;
 
+  /** 环境变量 */
   ctx: Envs & {};
 }
 
@@ -52,36 +53,39 @@ export abstract class FrontCli<T = any> {
     }
   }
 
-  beforeStep(context: T) {
+  customConfigStep(context: T) {
     return {
-      name: 'before_step',
+      name: 'custom_config_step',
       remark: ``,
       callback: async (ctx: any) => {
         const { __dirname } = this.context as any;
 
-        let customConfig: ICustomCliConfig = {
-          outputPrefixList: [],
+        let customConfig: CustomCliConfig = {
+          outputPrefixList: () => [],
           outputPathMap: () => ({}),
           beforePrompts: [],
           beforeData: () => ({}),
         };
-        let customConfigMap: { [key: string]: ICustomCliConfig } = {};
 
+        let outputPrefix = '';
+        let useOutputPathMap = false;
+        let customConfigMap: { [key: string]: CustomCliConfig } = {};
         let beforePromptsResult: { [key: string]: any } = {};
         let outputPathMap: { [key: string]: string } = {};
-        let outputPrefix = '';
 
         /** 载入自定义配置模块 */
         const customConfigModule = await import(
           path.join(__dirname, `${CLI_CONFIG_FILE_NAME}/${TEMPLATE_CONFIG_FILE_NAME}`)
         );
 
+        /** 如果导入配置函数正常, 则获取配置 */
         if (typeof customConfigModule.default === 'function') {
           customConfigMap = customConfigModule.default(this.context) as {
-            [key: string]: ICustomCliConfig;
+            [key: string]: CustomCliConfig;
           };
         }
 
+        /** 让用户选择自定义的配置对象 */
         if (Object.keys(customConfigMap).length) {
           const configChoices = Object.keys(customConfigMap).map((key) => ({
             title: key,
@@ -89,43 +93,50 @@ export abstract class FrontCli<T = any> {
           }));
 
           const configKey = await Prompt.autocomplete('Please select custom config', configChoices);
-
           if (configKey) customConfig = customConfigMap[configKey];
+        }
 
-          if (
-            Array.isArray(customConfig.outputPrefixList) &&
-            customConfig.outputPrefixList.length
-          ) {
-            const prefixChoices = customConfig.outputPrefixList.map((key) => ({
+        /** 根据配置的 outputPrefix 列表选择, 输出路径的前缀 */
+        if (typeof customConfig.outputPrefixList === 'function') {
+          const prefixList = customConfig.outputPrefixList(this.context);
+          if (Array.isArray(prefixList) && prefixList.length) {
+            const prefixChoices = prefixList.map((key) => ({
               title: key,
               value: key,
             }));
             outputPrefix = await Prompt.autocomplete('Please select custom prefix', prefixChoices);
           }
+        }
 
-          if (Array.isArray(customConfig.beforePrompts) && customConfig.beforeContext.length) {
-            beforePromptsResult = prompts(customConfig.beforePrompts);
-          }
+        /** 若设置了自定义问题则执行 */
+        if (Array.isArray(customConfig.beforePrompts) && customConfig.beforeContext.length) {
+          beforePromptsResult = prompts(customConfig.beforePrompts);
+        }
 
-          if (typeof customConfig.outputPathMap === 'function') {
-            outputPathMap = customConfig.outputPathMap({
-              ...customConfig.beforeData,
-              prefix: outputPrefix || '',
-              ...this.context,
-            });
+        /** 根据用户选饿的 outputPrefix 执行, 并返回输出路径映射集合 */
+        if (typeof customConfig.outputPathMap === 'function') {
+          outputPathMap = customConfig.outputPathMap({
+            ...customConfig.beforeData,
+            prefix: outputPrefix || '',
+            ...this.context,
+          });
 
-            if (Object.keys(outputPathMap).length) {
-            }
+          /** 若有输出路径集合, 则询问是否 直接使用输出路径集合 */
+          if (Object.keys(outputPathMap).length) {
+            useOutputPathMap = await Prompt.toggle(
+              'Whether to use outputPathMap as the output path',
+            );
           }
         }
 
         return {
           code: ResCode.next,
           data: {
-            beforeContext: {
-              outputPrefix,
+            customConfigStep: {
               ...beforePromptsResult,
+              outputPrefix,
               outputPathMap,
+              useOutputPathMap,
             },
           },
         };
