90e711f87fdda1e5de3c6511052d5717a074ea64
chen <92xiaodao@gmail.com>
2024-09-18 13:35:58 +0800

    feat: ✨ add custom config in before step

--- src/utils/core/front-cli.ts
+++ src/utils/core/front-cli.ts
@@ -1,18 +1,22 @@
-import { StepList, StepScheduler } from './scheduler';
+import { ResCode, StepList, StepScheduler } from './scheduler';
 import { Envs } from '@type/env';
 import { Logger } from '../logger/index';
+import { CLI_CONFIG_FILE_NAME, TEMPLATE_CONFIG_FILE_NAME } from '@constants/common';
+import path from 'path';
+import { Prompt, PromptList } from '@utils';
+import prompts from 'prompts';
 
-export interface CustomCliConfig<R = { [key: string]: any }, O = { [key: string]: any }> {
+export interface ICustomCliConfig {
   [key: string]: any;
 
-  /** */
+  /** 自定义输出前缀列表 */
   outputPrefixList: Array<string>;
-  /** */
-  outputPathMap: (ctx: O) => { [key: string]: string };
-  /** */
-  beforePrompts: Array<any>;
-  /** */
-  beforeContext: (ctx: R) => { [key: string]: any; outputPrefix?: string };
+  /** 自定义输出路径集合 */
+  outputPathMap: (ctx: any) => { [key: string]: string };
+  /** 自定义交互列表 */
+  beforePrompts: PromptList;
+  /** 自定义执行时所需的数据 */
+  beforeData: (ctx: any) => { [key: string]: any };
 }
 
 export interface IFrontCliOptions {
@@ -21,7 +25,7 @@ export interface IFrontCliOptions {
   ctx: Envs & {};
 }
 
-export abstract class FrontCli<T> {
+export abstract class FrontCli<T = any> {
   /** 存储入参 */
   options: IFrontCliOptions;
 
@@ -48,5 +52,84 @@ export abstract class FrontCli<T> {
     }
   }
 
-  initCustomConfig<R, O>(cliConfig: CustomCliConfig<R, O>) {}
+  beforeStep(context: T) {
+    return {
+      name: 'before_step',
+      remark: ``,
+      callback: async (ctx: any) => {
+        const { __dirname } = this.context as any;
+
+        let customConfig: ICustomCliConfig = {
+          outputPrefixList: [],
+          outputPathMap: () => ({}),
+          beforePrompts: [],
+          beforeData: () => ({}),
+        };
+        let customConfigMap: { [key: string]: ICustomCliConfig } = {};
+
+        let beforePromptsResult: { [key: string]: any } = {};
+        let outputPathMap: { [key: string]: string } = {};
+        let outputPrefix = '';
+
+        /** 载入自定义配置模块 */
+        const customConfigModule = await import(
+          path.join(__dirname, `${CLI_CONFIG_FILE_NAME}/${TEMPLATE_CONFIG_FILE_NAME}`)
+        );
+
+        if (typeof customConfigModule.default === 'function') {
+          customConfigMap = customConfigModule.default(this.context) as {
+            [key: string]: ICustomCliConfig;
+          };
+        }
+
+        if (Object.keys(customConfigMap).length) {
+          const configChoices = Object.keys(customConfigMap).map((key) => ({
+            title: key,
+            value: key,
+          }));
+
+          const configKey = await Prompt.autocomplete('Please select custom config', configChoices);
+
+          if (configKey) customConfig = customConfigMap[configKey];
+
+          if (
+            Array.isArray(customConfig.outputPrefixList) &&
+            customConfig.outputPrefixList.length
+          ) {
+            const prefixChoices = customConfig.outputPrefixList.map((key) => ({
+              title: key,
+              value: key,
+            }));
+            outputPrefix = await Prompt.autocomplete('Please select custom prefix', prefixChoices);
+          }
+
+          if (Array.isArray(customConfig.beforePrompts) && customConfig.beforeContext.length) {
+            beforePromptsResult = prompts(customConfig.beforePrompts);
+          }
+
+          if (typeof customConfig.outputPathMap === 'function') {
+            outputPathMap = customConfig.outputPathMap({
+              ...customConfig.beforeData,
+              prefix: outputPrefix || '',
+              ...this.context,
+            });
+
+            if (Object.keys(outputPathMap).length) {
+            }
+          }
+        }
+
+        return {
+          code: ResCode.next,
+          data: {
+            beforeContext: {
+              outputPrefix,
+              ...beforePromptsResult,
+              outputPathMap,
+            },
+          },
+        };
+      },
+    };
+  }
 }
